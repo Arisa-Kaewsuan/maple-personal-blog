ef1a1b636b46de369c8a92cba2c724367f58e66d
Arisa-Kaewsuan <Arisa.Kaewsuan@gmail.com>
2025-03-18 15:49:20 +0700

    fix: article section(blogPosts.map)

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -55,21 +55,10 @@ const ArticleSection = () => {
           </div>
         </div>
 
-        <div className='px-4 md:px-0'>
-          <div className="md:flex md:gap-5">
-            <BlogCard className="flex flex-1" image={blogPosts[0].image} category={blogPosts[0].category} title={blogPosts[0].title} description={blogPosts[0].description} author={blogPosts[0].author} date={blogPosts[0].date}/>
-            <BlogCard className="flex flex-1" image={blogPosts[1].image} category={blogPosts[1].category} title={blogPosts[1].title} description={blogPosts[1].description} author={blogPosts[1].author} date={blogPosts[1].date}/>
-          </div>
-
-          <div className="md:flex md:gap-5">
-            <BlogCard className="flex flex-1" image={blogPosts[2].image} category={blogPosts[2].category} title={blogPosts[2].title} description={blogPosts[2].description} author={blogPosts[2].author} date={blogPosts[2].date}/>
-            <BlogCard className="flex flex-1" image={blogPosts[3].image} category={blogPosts[3].category} title={blogPosts[3].title} description={blogPosts[3].description} author={blogPosts[3].author} date={blogPosts[3].date}/>
-          </div>
-
-          <div className="md:flex md:gap-5">
-            <BlogCard className="flex flex-1" image={blogPosts[4].image} category={blogPosts[4].category} title={blogPosts[4].title} description={blogPosts[4].description} author={blogPosts[4].author} date={blogPosts[4].date}/>
-            <BlogCard className="flex flex-1" image={blogPosts[5].image} category={blogPosts[5].category} title={blogPosts[5].title} description={blogPosts[5].description} author={blogPosts[5].author} date={blogPosts[5].date}/>
-          </div>
+        <div className='px-4 md:px-0 grid grid-cols-1 md:grid-cols-2 gap-4 mt-8'>
+          {blogPosts.map((post, index) => (
+            <BlogCard key={index} className="flex flex-1" image={post.image} category={post.category} title={post.title} description={post.description} author={post.author} date={post.date}/>
+          ))}
         </div>
 
         <a href="#" className='mt-12 md:mt-20 underline text-brown-600 text-b1 font-medium flex justify-center'>View more</a>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
         </div>
 
         {/* Image-Center Section */}
-        <div className='mt-10 md:mt-[3.75rem]'>
+        <div className='mt-10 md:mt-[3.75rem] flex items-center justify-center'>
           <img src="/hero-img.jpg" alt="black cat on a man's shoulder" className='w-[21.4375rem] md:w-[24.125rem] h-[29.375rem] md:h-[33.0625rem] object-cover rounded-2xl'/>
         </div>
 
